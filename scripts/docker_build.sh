9714c31ec15c3b561f11686ea0bfb5c4ee4a5a26
martibayoalemany <martibayoalemany@gmail.com>
2017-06-11 14:30:30 +0200

    Check visual studio code snippets

---
+++ scripts/docker_build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit
+PROJECT_ROOT=${1:?You must pass the path to your file Dockerfile}
+PROJECT_ROOT=$(realpath ${PROJECT_ROOT})
+[[ $? == 0 ]]; PROJECT_NAME=$(realpath $0 | sed 's/.*\/\(.*\)\/.*\/.*/\1/')
+
+if [ $? == 0 ]
+then        
+    echo PROJECT_ROOT: $PROJECT_ROOT
+    echo PROJECT_NAME: $PROJECT_NAME
+    docker build ${PROJECT_ROOT} -t ${PROJECT_NAME}
+    docker run -v `pwd`:/git -P -it ${PROJECT_NAME} bash 
+fi
